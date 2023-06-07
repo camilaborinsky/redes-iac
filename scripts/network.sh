f90320d921a75d12e40bd637e7aa17c976c5a107
camilaborinsky <cborinsky@itba.edu.ar>
2023-06-07 13:27:39 -0300

    Create script for network and .env for credentials

---
+++ scripts/network.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+cd "$SCRIPT_DIR/../terraform"
+
+export $(cat .env | xargs)
+
+terraform init
+terraform apply -auto-approve
